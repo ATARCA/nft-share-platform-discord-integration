c99375118c86431b03b0a5854eb75f000db3c914
Jarno <jarno.marttila@sujuwa.fi>
2022-12-13 15:38:54 +0100

    refactoring to lambda function

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ const _ = require('lodash')
 const {WebhookClient, EmbedBuilder} = require('discord.js')
 
 const API_URL = process.env.API_URL
-const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
 const WEBHOOK_URL = process.env.WEBHOOK_URL
 const PROJECT = process.env.PROJECT
-const UPDATE_IF_ORIGINAL_AWARD=true
-const UPDATE_IF_SHARED_AWARD=false
-const UPDATE_IF_LIKE=false
-
 const TEMPORARY_BLOCK_LIMIT = 36448364
 //Todo: rename temporary block limit to something more representative, e.g. last block checked
 //Retrieve last block checked from database
@@ -28,7 +23,6 @@ const aws_remote_config = {
 
 AWS.config.update(aws_remote_config)
 const client = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
-//const client = new DynamoDBClient(aws_remote_config);
 
 const params = {
     TableName: 'talko-discord-bot',
@@ -77,7 +71,7 @@ const projectSpecificTokens = gql`
 
 async function getMetadataObject(metadataUri) {
     try {
-        console.log('attempting to fetch url', metadataUri)
+        //console.log('attempting to fetch url', metadataUri)
         const response = await fetch(metadataUri)
         return response
     } catch(error) {
@@ -98,15 +92,11 @@ async function parseAndPostToChannel(object) {
     //Todo move client intilization outside the function, inefficient to keep initializing it
     const talkoTokenURI = 'https://talkoapp.io/token/' + object?.contractAddress + '/' + object?.tokenId
     const metadataUri = object.metadataUri
-    //move metadata loading to separat function, metadata loading can fail with 403 code if consent has not been given
-    //json body may be invalid if metadata is not available, handle regression gracefully
     const metadataResponse = await getMetadataObject(metadataUri)
     const tokenType = object?.isOriginal ? 'Original Award' : (object?.isSharedInstance ? 'Shared Award' : 'Like');
     const embed = new EmbedBuilder()
-    //let data = {}
     if (metadataResponse?.ok) {
         const data = await metadataResponse.json();
-
         embed
         .setTitle('New award token has been minted')
         .setColor(0x00FFFF)
@@ -123,12 +113,6 @@ async function parseAndPostToChannel(object) {
     
     const metadataAvailable = metadataResponse.status == 200 ? true : false;
     const metadataNotAvailable = 'Award receiver has not yet consented the publication of the metadata. If this is your award, please check your consent options on talkopp.io for the receiving wallet.'
-    //fetch metadata contents
-    //if metadata has not yet been authorized to be shown, instruct the receiver to head to platform to show it
-    
-    //is the minted token an award token, a like token or a shared award token
-
-    //Todo: add a bit more detail about it, e.g. received address, and a link to talkoplatform where to add token consent
     const embedNoMetadata = new EmbedBuilder()
         .setTitle('New award token has been minted')
         .setDescription(metadataNotAvailable)
@@ -139,47 +123,38 @@ async function parseAndPostToChannel(object) {
         )
 
     try {
-        webhookClient.send({
+        console.log('attempting to send data to discord', object?.tokenId)
+        console.log('webhookclient', webhookClient.token)
+        const result = await webhookClient.send({
             content: '',
             username: 'Talko-Bot',
             avatarURL: 'https://images.talkoapp.io/discord_bot_1.png',
             embeds: metadataAvailable ? [embed] : [embedNoMetadata],
         });
+        console.log('msg succesfully sent to channel at ', result?.timestamp)
     } catch(error) {
         console.log('Failed to send update to channel', error)
-    }
-
-    
+    }    
 }
 
 //Webhook initialization
-//try catch sending the msg, succesful update dynamodb
-// to be refactored as a periodically executed aws lambda function
-//check from cache what was the latest token if any, what was the latest mintblock checked, check if any new tokens minted since then
-//consider aws dynamodb as a cache
-//check if there are new tokens for streamr project
-// if there is a new token send it to discord bot
-//36448370
-
 async function main() {
 
     //fetch blockheight from dynamodb
     //if blockgheight not set, use default blockheight
     const item = await getItem()
-    const blockheight = 'blockheight' in item ? item.blockheight : TEMPORARY_BLOCK_LIMIT;
-    
-    //const blockheight = 36448370
-    //is original, is shared, is liked
-    //filter accordingly
-
-    const client = createClient({
+    console.log('receivedblockheight from dynamodb', item?.blockheight)
+    //const blockheight = 'blockheight' in item ? item.blockheight : TEMPORARY_BLOCK_LIMIT;
+    const blockheight = 36448364
+    console.log('Blockheight: ', blockheight)
+    const graphQLClient = createClient({
         url: API_URL,
         fetch: fetch,
     })
     
     //Todo: update query to include blockheight as parameter
-    const data = await client.query(projectSpecificTokens, {project: PROJECT}).toPromise()
-    
+    const data = await graphQLClient.query(projectSpecificTokens, {project: PROJECT}).toPromise()
+
     //find latest mintBlock
 
     const latestMintBlockObject = _.maxBy(data?.data?.tokens, function(o){
@@ -201,8 +176,8 @@ async function main() {
     console.log('New tokens',origOrShared)
 
     //each discord update is done asynchronously, the order or updates is not guaranteed
-    _.forEach(origOrShared, function(token) {
-        parseAndPostToChannel(token)
+    _.forEach(origOrShared, async function(token) {
+        await parseAndPostToChannel(token)
     })
 
     const updateParams = {
@@ -220,4 +195,9 @@ async function main() {
     //update checked block limit with a new block number, last item on the list is the highest last check block 
 }
 
-main()
+//main()
+
+exports.handler = async (event) => {
+    console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
+    await main()    
+};
