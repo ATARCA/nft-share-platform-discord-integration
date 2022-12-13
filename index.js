deea10a96dc8eaf9fc316f3544e4d2c2e7224dfd
Jarno <jarno.marttila@sujuwa.fi>
2022-12-13 11:32:30 +0100

    WiP

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 require('dotenv').config();
 //use AWS secrets once refactored as a lambda function
 const AWS = require('aws-sdk')
-const { DynamoDBClient, BatchExecuteStatementCommand } = require("@aws-sdk/client-dynamodb");
 const { createClient, gql } = require('@urql/core')
 const fetch = require('node-fetch')
 const _ = require('lodash')
-const Discord = require('discord.js')
 const {WebhookClient, EmbedBuilder} = require('discord.js')
 
 const API_URL = process.env.API_URL
@@ -57,7 +55,7 @@ async function updateItem(params) {
 
 const projectSpecificTokens = gql`
     query TokenQuery($project: String!) {
-        tokens(orderBy: mintBlock, orderDirection: desc, where: {project: $project}) {
+        tokens(orderBy: mintBlock, orderDirection: asc, where: {project: $project}) {
             tokenId
             contractAddress
             isLikeToken
@@ -103,10 +101,11 @@ async function parseAndPostToChannel(object) {
     //move metadata loading to separat function, metadata loading can fail with 403 code if consent has not been given
     //json body may be invalid if metadata is not available, handle regression gracefully
     const metadataResponse = await getMetadataObject(metadataUri)
+    const tokenType = object?.isOriginal ? 'Original Award' : (object?.isSharedInstance ? 'Shared Award' : 'Like');
     const embed = new EmbedBuilder()
-    let data = {}
+    //let data = {}
     if (metadataResponse?.ok) {
-        data = await metadataResponse.json();
+        const data = await metadataResponse.json();
 
         embed
         .setTitle('New award token has been minted')
@@ -117,6 +116,7 @@ async function parseAndPostToChannel(object) {
         .addFields(
             { name: 'Receiver', value: object?.ownerAddress },
             { name: 'Award Category', value: object?.category?.id },
+            { name: 'Token Type', value: tokenType },
             { name: 'Link', value: '[Talkoapp.io]('+talkoTokenURI+')' }
         )
     }
@@ -126,12 +126,17 @@ async function parseAndPostToChannel(object) {
     //fetch metadata contents
     //if metadata has not yet been authorized to be shown, instruct the receiver to head to platform to show it
     
-
     //is the minted token an award token, a like token or a shared award token
 
+    //Todo: add a bit more detail about it, e.g. received address, and a link to talkoplatform where to add token consent
     const embedNoMetadata = new EmbedBuilder()
         .setTitle('New award token has been minted')
         .setDescription(metadataNotAvailable)
+        .addFields(
+            { name: 'Receiver', value: object?.ownerAddress },
+            { name: 'Token Type', value: tokenType },
+            { name: 'Link', value: '[Talkoapp.io](https://talkoapp.io/)' }
+        )
 
     try {
         webhookClient.send({
@@ -144,16 +149,7 @@ async function parseAndPostToChannel(object) {
         console.log('Failed to send update to channel', error)
     }
 
-    const updateParams = {
-        TableName: 'talko-discord-bot',
-        Key: {"id":"production-blockheight"},
-        UpdateExpression: 'set blockheight = :v',
-        ExpressionAttributeValues: {
-            ':v': _.parseInt(object?.mintBlock)
-        }
-    }
-
-    await updateItem(updateParams)
+    
 }
 
 //Webhook initialization
@@ -163,6 +159,7 @@ async function parseAndPostToChannel(object) {
 //consider aws dynamodb as a cache
 //check if there are new tokens for streamr project
 // if there is a new token send it to discord bot
+//36448370
 
 async function main() {
 
@@ -170,26 +167,54 @@ async function main() {
     //if blockgheight not set, use default blockheight
     const item = await getItem()
     const blockheight = 'blockheight' in item ? item.blockheight : TEMPORARY_BLOCK_LIMIT;
+    
+    //const blockheight = 36448370
+    //is original, is shared, is liked
+    //filter accordingly
 
     const client = createClient({
         url: API_URL,
         fetch: fetch,
-      })
+    })
     
     //Todo: update query to include blockheight as parameter
     const data = await client.query(projectSpecificTokens, {project: PROJECT}).toPromise()
+    
+    //find latest mintBlock
+
+    const latestMintBlockObject = _.maxBy(data?.data?.tokens, function(o){
+        return _.parseInt(o.mintBlock)
+    })
+
+    const latestMintBlock = latestMintBlockObject?.mintBlock
     
     //filter out all tokens that are newer than the last checked mintblock
     const newTokens = _.filter(data?.data?.tokens, function(o) {
         return _.parseInt(o.mintBlock) > blockheight
     })
 
-    console.log('New tokens',newTokens)
+    //filter out all tokens that are original or shared awards
+    const origOrShared = _.filter(newTokens, function(o) {
+        return o.isOriginal || o.isSharedInstance
+    })
 
-    _.forEach(newTokens, function(token) {
+    console.log('New tokens',origOrShared)
+
+    //each discord update is done asynchronously, the order or updates is not guaranteed
+    _.forEach(origOrShared, function(token) {
         parseAndPostToChannel(token)
     })
 
+    const updateParams = {
+        TableName: 'talko-discord-bot',
+        Key: {"id":"production-blockheight"},
+        UpdateExpression: 'set blockheight = :v',
+        ExpressionAttributeValues: {
+            ':v': _.parseInt(latestMintBlock)
+        }
+    }
+
+    await updateItem(updateParams)
 
     //push new token information as message to discord bot
     //update checked block limit with a new block number, last item on the list is the highest last check block 
